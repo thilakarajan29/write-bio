aee111de13021b44b6e9c135cc1efb719b0b4068
Thilak <thilakarajanvenkat@gmail.com>
2023-10-30 12:10:12 +0530

    app.js

--- src/bottomnav.js
+++
@@ -1,8 +0,0 @@
-const BottomNav = () => {
-    return ( <nav className="navbar-bottom-bio">
-        <button className="back-bio">Back</button>
-        <button className="add-bio">Next, choose your categories</button>
-    </nav> );
-}
- 
-export default BottomNav;
